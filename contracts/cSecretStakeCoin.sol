e7a9b3c3e74baa0834e8636ad2dbdd420a8c75aa
DoingB007 <hjyeapkfvb76@hotmail.com>
2025-08-11 13:52:07 +0800

    mint cSSC

--- contracts/cSecretStakeCoin.sol
+++ contracts/cSecretStakeCoin.sol
@@ -1,11 +1,22 @@
 // SPDX-License-Identifier: BSD-3-Clause-Clear
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.27;
 
-import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 import {ConfidentialFungibleToken} from "@openzeppelin/confidential-contracts/token/ConfidentialFungibleToken.sol";
-import {ConfidentialFungibleToken} from "@openzeppelin/confidential-contracts/token/ConfidentialFungibleToken.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {SepoliaConfig} from "@fhevm/solidity/config/ZamaConfig.sol";
+import {FHE, euint64, externalEuint64} from "@fhevm/solidity/lib/FHE.sol";
 
-contract cSecretStakeCoin is ConfidentialFungibleToken, SepoliaConfig {
-    constructor() ConfidentialFungibleToken("cSecretStakeCoin", "cSSC", "") {}
+contract CSecretStakeCoin is ConfidentialFungibleToken, Ownable, SepoliaConfig {
+    constructor() ConfidentialFungibleToken("cSecretStakeCoin", "cSSC", "") Ownable(msg.sender) {}
+    
+    function mint(address to, externalEuint64 encryptedAmount, bytes calldata inputProof) external onlyOwner {
+        euint64 amount = FHE.fromExternal(encryptedAmount, inputProof);
+        _mint(to, amount);
+    }
+    
+    // Overload for internal use with plain amounts
+    function mintPlain(address to, uint256 amount) external onlyOwner {
+        euint64 encryptedAmount = euint64.wrap(bytes32(amount));
+        _mint(to, encryptedAmount);
+    }
 }
